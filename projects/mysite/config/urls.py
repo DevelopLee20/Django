d9dd7e938ab0795c0f00f7583f383dc2a80309e2
LeeInGyu <developLee20@gmail.com>
2021-11-27 05:34:34 +0900

    URL 분리하기, 2-1장 완료

--- projects/mysite/config/urls.py
+++ projects/mysite/config/urls.py
@@ -14,7 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+# include 함수는 다른 주소를 include 할 때 사용한다.
+from django.urls import path, include
 # 화면을 보여주기 위한 views 함수 불러오기
 from pybo import views
 
@@ -22,7 +23,12 @@ from pybo import views
 # URL 끝에는 / 를 붙이는 것이 일반적이다.
 urlpatterns = [
     path('admin/', admin.site.urls),
+    
     # pybo라는 URL 주소를 추가 index 함수를 화면에 보여줌
     # path 함수를 사용해 pybo/ URL과 views.index를 매핑
-    path('pybo/', views.index),
+    # 이 곳에서 이 코드는 프로젝트의 짜임새를 전혀 고려하지 않은 방식
+    # path('pybo/', views.index),
+
+    # pybo/ 로 시작되는 페이지 요청은 모두 pybo.urls 파일에 있는 URL 매핑을 참조하라는 의미이다.
+    path('pybo/', include('pybo.urls')),
 ]
---
+++ projects/mysite/pybo/urls.py
@@ -0,0 +1,9 @@
+# 위치 참조 관련 라이브러리
+from django.urls import path
+# . (같은 폴더)의 views.py 파일 import
+from . import views
+
+urlpatterns = [
+    # path 함수를 사용해 pybo/ URL과 views.index를 매핑
+    path('', views.index),
+]
