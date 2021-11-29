43f1139fe2ddabd24fd20d6075f5c44fb2021914
LeeInGyu <developLee20@gmail.com>
2021-11-30 03:34:01 +0900

    2-4 02단계

--- projects/mysite/pybo/views.py
+++ projects/mysite/pybo/views.py
@@ -1,6 +1,12 @@
-from django.shortcuts import render
 # 페이지 요청에 의한 응답을 할 때 사용하는 클래스
 from django.http import HttpResponse
+# Question 클래스 참조
+from .models import Question
+# 모델 데이터를 HTML로 변환
+from django.shortcuts import render
 
 def index(request):
-    return HttpResponse("안녕하세요 pybo에 오신것을 환영합니다.")
+    # order_by : 특정 속성 기준으로 특정 속성을 정렬, create_date: 작성일시, -create_date: 작성일시 역순
+    question_list = Question.objects.order_by('-create_date')
+    context = {'question_list' : question_list}
+    return render(request, 'pybo/question_list.html', context)
