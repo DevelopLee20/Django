be22fa0b0180e52872006c02b4dd54b55a7d052e
LeeInGyu <developLee20@gmail.com>
2021-12-03 00:24:27 +0900

    2-5 URL 별칭으로 URL 하드 코딩 문제 해결하기 완료

--- projects/mysite/pybo/urls.py
+++ projects/mysite/pybo/urls.py
@@ -3,9 +3,14 @@ from django.urls import path
 # . (같은 폴더)의 views.py 파일 import
 from . import views
 
+# app_name : namespace 설정
+# namespace : 앱이 관리하는 독립된 이름 공간
+app_name = 'pybo'
+
 urlpatterns = [
     # path 함수를 사용해 pybo/ URL과 views.index를 매핑
-    path('', views.index),
+    # name='index', index를 URL 별칭으로 사용
+    path('', views.index, name='index'),
     # path 함수를 사용해 pybo/ URL과 views.detail를 매핑
-    path('<int:question_id>/', views.detail),
+    path('<int:question_id>/', views.detail, name='detail'),
 ]
